2a8347eaf1a5b091dd44216d3a31ccebb65171bf
HDR <refshet@gmail.com>
2024-03-18 21:57:36 +0100

    Update ChannelCreate.js

--- events/ChannelCreate.js
+++ events/ChannelCreate.js
@@ -3,7 +3,6 @@ const {Events, EmbedBuilder, AuditLogEvent, ChannelType} = require("discord.js")
 const servers = require("../servers.json")
 
 client.on(Events.ChannelCreate, async(GuildChannel) => {
-
     const auditLog = await GuildChannel.guild.fetchAuditLogs({
         limit: 1,
         type: AuditLogEvent.ChannelCreate,
