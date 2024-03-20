8f5081c841d19fca88bc9e0ad44e5a2af185e7d3
HDR <refshet@gmail.com>
2024-03-20 19:00:22 +0100

    Update ChannelUpdate.js
    - Fixes a bug that triggers the code for Channel permission changes when the channel topic is updated

--- events/ChannelUpdate.js
+++ events/ChannelUpdate.js
@@ -19,124 +19,125 @@ client.on(Events.ChannelUpdate, async(OldGuildChannel, NewGuildChannel) => {
     Embed.setColor('#97ff28');
 
     let audit = await OldGuildChannel.guild.fetchAuditLogs({
-        limit: 1,
-        type: AuditLogEvent.ChannelUpdate
+        limit: 1
     });
 
-    let auditLog = await OldGuildChannel.guild.fetchAuditLogs({
-        limit: 1,
-    });
+    let { executor, changes, extra } = audit.entries.first();
 
-    let { executor, changes, extra } = auditLog.entries.first();
+    if (audit.entries.first().action === AuditLogEvent.ChannelUpdate) {
+        for (const [key, value] of Object.entries(getObjectDiffKey(OldGuildChannel, NewGuildChannel))) {
+            switch (value) {
 
-    for (const [key, value] of Object.entries(getObjectDiffKey(OldGuildChannel, NewGuildChannel))) {
-        switch (value) {
+                default:
+                    console.log(value)
+                    break;
 
-            default:
-                console.log(value)
-                break;
+                case 'topic':
+                    Embed.addFields({
+                        name: 'Topic',
+                        value: `Old Topic: \`${OldGuildChannel.topic}\`\nNew Topic: \`${NewGuildChannel.topic}\``
+                    })
+                    break;
 
-            case 'topic':
-                Embed.addFields({
-                    name: 'Topic',
-                    value: `Old Topic: \`${OldGuildChannel.topic}\`\nNew Topic: \`${NewGuildChannel.topic}\``
-                })
-                break;
+                case 'nsfw':
+                    Embed.addFields({
+                        name: 'NSFW',
+                        value: `Old State: \`${OldGuildChannel.nsfw}\`\nNew State: \`${NewGuildChannel.nsfw}\``
+                    })
+                    break;
 
-            case 'nsfw':
-                Embed.addFields({
-                    name: 'NSFW',
-                    value: `Old State: \`${OldGuildChannel.nsfw}\`\nNew State: \`${NewGuildChannel.nsfw}\``
-                })
-                break;
+                case 'rateLimitPerUser':
+                    Embed.addFields({
+                        name: 'Slowmode',
+                        value: `Old Rate Limit: \`${OldGuildChannel.rateLimitPerUser}\`\nNew Rate Limit: \`${NewGuildChannel.rateLimitPerUser}\``
+                    })
+                    break;
 
-            case 'rateLimitPerUser':
-                Embed.addFields({
-                    name: 'Slowmode',
-                    value: `Old Rate Limit: \`${OldGuildChannel.rateLimitPerUser}\`\nNew Rate Limit: \`${NewGuildChannel.rateLimitPerUser}\``
-                })
-                break;
-
-            case 'defaultAutoArchiveDuration':
-                Embed.addFields({
-                    name: 'Thread auto hide interval',
-                    value: `Old Interval: \`${OldGuildChannel.defaultAutoArchiveDuration}\`\nNew Interval: \`${NewGuildChannel.defaultAutoArchiveDuration}\``
-                })
-                break;
+                case 'defaultAutoArchiveDuration':
+                    Embed.addFields({
+                        name: 'Thread auto hide interval',
+                        value: `Old Interval: \`${OldGuildChannel.defaultAutoArchiveDuration}\`\nNew Interval: \`${NewGuildChannel.defaultAutoArchiveDuration}\``
+                    })
+                    break;
 
 
-            case 'permissionOverwrites':
-                //Ignore this for now
-                break;
+                case 'permissionOverwrites':
+                    //Ignore this for now
+                    break;
+            }
         }
     }
 
-    if (auditLog.entries.first().action === AuditLogEvent.ChannelOverwriteCreate || AuditLogEvent.ChannelOverwriteUpdate || AuditLogEvent.ChannelOverwriteDelete) {
-        let target;
-        let typeString;
-        if(extra.name) {
-            typeString = `GuildRole`
-            target = extra.name
-        } else {
-            typeString = `User`
-            target = `<@${extra.id}>`
-        }
+    console.log(changes[0].key)
 
-        changes.forEach((item, index) => {
-            if (changes[0].key === 'id') {
-                if(changes[0].old === undefined && index === 0) {
-                    Embed.addFields(
-                        {
-                            name: `A Permission target was added`,
-                            value: `${typeString}: ${target} (${extra.id})`
-                        }
-                    )
-                }
-                if(changes[0].new === undefined && index === 0){
-                    Embed.addFields(
-                        {
-                            name: `A Permission target was removed`,
-                            value: `${typeString}: ${target} (${extra.id})`
-                        }
-                    )
-                }
-
+    if(changes[0].key === "id") {
+        if (audit.entries.first().action === AuditLogEvent.ChannelOverwriteCreate || AuditLogEvent.ChannelOverwriteUpdate || AuditLogEvent.ChannelOverwriteDelete) {
+            let target;
+            let typeString;
+            if(extra.name) {
+                typeString = `GuildRole`
+                target = extra.name
             } else {
-                if(index === 0){
-                    try {
-                        let newPerm = permissionResolver(new PermissionsBitField(changes[0].new))
-                        let oldPerm = permissionResolver(new PermissionsBitField(changes[0].old))
+                typeString = `User`
+                target = `<@${extra.id}>`
+            }
 
-                        let newString = "";
-                        let oldString = "";
-
-                        for (let [key, value] of Object.entries(newPerm)) {
-                            newString += `\nAllow: ${value}`
-                        }
-
-                        for (let [key, value] of Object.entries(oldPerm)) {
-                            oldString += `\nDeny: ${value}`
-                        }
-
-                        if (newString.length === 0) {
-                            newString = ""
-                        }
-                        if (oldString.length === 0) {
-                            oldString = ""
-                        }
-
+            changes.forEach((item, index) => {
+                if (changes[0].key === 'id') {
+                    if(changes[0].old === undefined && index === 0) {
                         Embed.addFields(
                             {
-                                name: `Permissions`,
-                                value: `${newString}\n ${oldString}`
+                                name: `A Permission target was added`,
+                                value: `${typeString}: ${target} (${extra.id})`
                             }
                         )
-                    } catch (e) {
-                        console.log(e)
+                    }
+                    if(changes[0].new === undefined && index === 0){
+                        Embed.addFields(
+                            {
+                                name: `A Permission target was removed`,
+                                value: `${typeString}: ${target} (${extra.id})`
+                            }
+                        )
+                    }
+
+                } else {
+                    if(index === 0){
+                        try {
+                            let newPerm = permissionResolver(new PermissionsBitField(changes[0].new))
+                            let oldPerm = permissionResolver(new PermissionsBitField(changes[0].old))
+
+                            let newString = "";
+                            let oldString = "";
+
+                            for (let [key, value] of Object.entries(newPerm)) {
+                                newString += `\nAllow: ${value}`
+                            }
+
+                            for (let [key, value] of Object.entries(oldPerm)) {
+                                oldString += `\nDeny: ${value}`
+                            }
+
+                            if (newString.length === 0) {
+                                newString = ""
+                            }
+                            if (oldString.length === 0) {
+                                oldString = ""
+                            }
+
+                            Embed.addFields(
+                                {
+                                    name: `Permissions`,
+                                    value: `${newString}\n ${oldString}`
+                                }
+                            )
+                        } catch (e) {
+                            console.log(e)
+                        }
                     }
                 }
-            }
-        })
+            })
+        }
     }
 
 
