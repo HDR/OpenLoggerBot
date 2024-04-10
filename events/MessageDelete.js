163570979cc648a5a2582ecb8612b09b1956f1a3
HDR <refshet@gmail.com>
2024-04-10 20:50:31 +0200

    Update MessageDelete.js
    - Fixed an issue that caused embeds for deleted attachments to not stack

--- events/MessageDelete.js
+++ events/MessageDelete.js
@@ -50,8 +50,8 @@ client.on(Events.MessageDelete, async(Message) => {
         if(await eventState(Message.guildId, 'messageDeleteAttachments')) {
             if(Message.attachments.size > 0) {
                 let embeds = new Collection();
-                Message.attachments.forEach(function(attachment) {
-                    embeds.set(attachment.id, new EmbedBuilder().setColor('#ae3ffd').setDescription(`Attachments for deleted message in <#${Message.channel.id}>`).setAuthor({name: `${Message.author.username}#${Message.author.discriminator}`, iconURL: `${Message.author.displayAvatarURL()}`}).addFields( { name: 'ID', value: `\`\`\`ansi\n[0;33mMember = ${Message.author.id}\n[0;32mMessage = ${Message.id}\`\`\`` }).setImage(attachment.url).setFooter({text: `${client.user.tag}`, iconURL: `${client.user.displayAvatarURL()}`}).setTimestamp())
+                await Message.attachments.forEach(function(attachment) {
+                    embeds.set(attachment.id, new EmbedBuilder().setColor('#ae3ffd').setDescription(`Attachments for deleted message in <#${Message.channel.id}>`).setAuthor({name: `${Message.author.username}#${Message.author.discriminator}`, iconURL: `${Message.author.displayAvatarURL()}`}).addFields( { name: 'ID', value: `\`\`\`ansi\n[0;33mMember = ${Message.author.id}\n[0;32mMessage = ${Message.id}\`\`\`` }).setURL(`https://${Message.id}.notarealurl`).setImage(attachment.url).setFooter({text: `${client.user.tag}`, iconURL: `${client.user.displayAvatarURL()}`}).setTimestamp())
                 })
                 try {
                     await Message.guild.channels.cache.get(await eventState(Message.guildId, 'logChannel')).send({embeds: Array.from(embeds.values())});
