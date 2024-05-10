4ef3c8eb6c7388792aeab90a0bbc77c77d0268a7
HDR <refshet@gmail.com>
2024-05-10 14:07:20 +0200

    Fix #1
    - Fixes mute, deafen and video incorrectly being logged as Join Channel
    - Moving channels is now logged in a better way

--- events/VoiceStateUpdate.js
+++ events/VoiceStateUpdate.js
@@ -9,12 +9,31 @@ client.on(Events.VoiceStateUpdate, async(OldState, NewState) => {
             Embed.setAuthor({name: `${NewState.member.user.tag}`, iconURL: `${NewState.member.user.displayAvatarURL()}`})
             Embed.setFooter({text: `${client.user.tag}`, iconURL: `${client.user.displayAvatarURL()}`})
             Embed.setTimestamp()
-            if(NewState.channelId !== null && !NewState.member.user.bot) {
-                Embed.setDescription(`**${NewState.member.user.tag}** joined voice channel ${NewState.channel.name}`)
+
+            if(NewState.channelId !== null && OldState.channelId !== null && NewState.channelId !== OldState.channelId) {
+                Embed.setDescription(`**${NewState.member.user.tag}** switched voice channels from ${OldState.channel} to ${NewState.channel}`)
+                Embed.addFields(
+                    {
+                        name: 'Old Channel',
+                        value: `${OldState.channel}`
+                    },
+                    {
+                        name: 'New Channel',
+                        value: `${NewState.channel}`
+                    },
+                    {
+                        name: 'ID',
+                        value: `\`\`\`ansi\n[0;33mMember = ${NewState.member.id}\n[0;35mOld Channel = ${OldState.channel.id}\n[0;34mNew Channel = ${NewState.channel.id}\`\`\``
+                    }
+                )
+            }
+
+            if(!NewState.member.user.bot && NewState.channelId !== null && OldState.channelId === null && NewState.channelId !== OldState.channelId) {
+                Embed.setDescription(`**${NewState.member.user.tag}** joined voice channel ${NewState.channel}`)
                 Embed.addFields(
                     {
                         name: 'Channel',
-                        value: `<#${NewState.channel.id}> (${NewState.channel.name})`
+                        value: `${NewState.channel}`
                     },
                     {
                         name: 'ID',
@@ -23,12 +42,12 @@ client.on(Events.VoiceStateUpdate, async(OldState, NewState) => {
                 )
             }
 
-            if(NewState.channelId == null && !NewState.member.user.bot) {
-                Embed.setDescription(`**${NewState.member.user.tag}** left voice channel ${OldState.channel.name}`)
+            if(!NewState.member.user.bot && NewState.channelId === null & OldState.channelId !== null & NewState.channelId !== OldState.channelId){
+                Embed.setDescription(`**${NewState.member.user.tag}** left voice channel ${OldState.channel}`)
                 Embed.addFields(
                     {
                         name: 'Channel',
-                        value: `<#${OldState.channel.id}> (${OldState.channel.name})`
+                        value: `${OldState.channel}`
                     },
                     {
                         name: 'ID',
@@ -37,11 +56,13 @@ client.on(Events.VoiceStateUpdate, async(OldState, NewState) => {
                 )
             }
 
-            try {
-                await NewState.guild.channels.cache.get(await eventState(NewState.guild.id, 'logChannel')).send({embeds: [Embed]});
-            } catch (e) {
-                e.guild = NewState.guild
-                console.log(e)
+            if(Embed.data.fields) {
+                try {
+                    await NewState.guild.channels.cache.get(await eventState(NewState.guild.id, 'logChannel')).send({embeds: [Embed]});
+                } catch (e) {
+                    e.guild = NewState.guild
+                    console.log(e)
+                }
             }
         }
     }
