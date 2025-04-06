6010ce5ae57d6c7fa49174d9dca6eb120e2cb7cc
HDR <refshet@gmail.com>
2025-04-06 20:49:06 +0200

    Update MessageDelete.js
    - MessageDelete.js: Change the way we handle deleted attachments, instead of re-using the attachment url which expires we now re-upload them as new attachments

--- events/MessageDelete.js
+++ events/MessageDelete.js
@@ -50,13 +50,25 @@ client.on(Events.MessageDelete, async(Message) => {
             if(await eventState(Message.guildId, 'messageDeleteAttachments')) {
                 if(Message.attachments.first().contentType !== 'audio/ogg') {
                     let embeds = new Collection();
-                    await Message.attachments.forEach(function(attachment) {
-                        embeds.set(attachment.id, new EmbedBuilder().setColor('#ae3ffd').setDescription(`Attachments for deleted message in <#${Message.channel.id}>`).setAuthor({name: `${Message.author.username}#${Message.author.discriminator}`, iconURL: `${Message.author.displayAvatarURL()}`}).addFields({name: 'ID', value: `\`\`\`ansi\n[0;33mMember = ${Message.author.id}\n[0;32mMessage = ${Message.id}\`\`\``}).setURL(`https://${Message.id}.notarealurl`).setImage(attachment.url).setFooter({text: `${client.user.tag}`, iconURL: `${client.user.displayAvatarURL()}`}).setTimestamp())
-                    })
-                    try {
-                        await Message.guild.channels.cache.get(await eventState(Message.guildId, 'logChannel')).send({embeds: Array.from(embeds.values())});
-                    } catch (e) {
-                        console.log(e)
+                    let uploadedAttachments = [];
+                    for (const attachment of Message.attachments.values()) {
+                        try {
+                            const res = await fetch(attachment.url);
+                            const arrayBuffer = await res.arrayBuffer();
+                            const buffer = Buffer.from(arrayBuffer);
+                            const fileName = attachment.name || 'attachment.png';
+                            const upldAttachment = new AttachmentBuilder(buffer, { name: fileName });
+                            uploadedAttachments.push(upldAttachment);
+                            embeds.set(attachment.id, new EmbedBuilder().setColor('#ae3ffd').setDescription(`Attachments for deleted message in <#${Message.channel.id}>`).setAuthor({name: `${Message.author.tag}`, iconURL: `${Message.author.displayAvatarURL()}`}).addFields({name: 'ID', value: `\`\`\`ansi\n[0;33mMember = ${Message.author.id}\n[0;32mMessage = ${Message.id}\`\`\``}).setURL(`https://${Message.id}.notarealurl`).setImage(`attachment://${fileName}`).setFooter({text: `${client.user.tag}`, iconURL: `${client.user.displayAvatarURL()}`}).setTimestamp())
+
+                        } catch (e) {
+                            e.guild = Message.guild;
+                            console.log(e)
+                        }
+                    }
+
+                    if (uploadedAttachments.length > 0) {
+                        await Message.guild.channels.cache.get(await eventState(Message.guildId, 'logChannel')).send({embeds: Array.from(embeds.values()), files: uploadedAttachments});
                     }
                 }
             }
